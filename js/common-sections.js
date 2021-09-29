c903a0e433a06e668b6ee02558786ea315aac9fc
Venkatesh Pallanti <venkateshpa@infinite.com>
2021-09-29 23:42:17 +0530

    stepper functionality added on button click next & previous

--- js/common-sections.js
+++ js/common-sections.js
@@ -4,8 +4,37 @@ $(document).ready(
        $("#mainHeader").load("main-header.html");
        $("#mainRightSection").load("main-right-section.html");
        $("#mainFooter").load("main-footer.html");
-
+       $("#chooseTopic").show();
       // alert(location.pathname.search('my-claims.html'));
-    }
-    
-)
+    }  
+);
+
+function goNext(selectedValue,classRemove,classAdd){
+  // Show Hide
+  $("#chooseTopic").hide();
+  $("#customizeEstimate").hide();
+  $("#viewEstimate").hide(); 
+  $('#'+selectedValue).show();
+  // Show Hide end
+  // Color Class Removing
+  $('#'+classRemove).removeClass("btn-success");
+  // Color Class Adding
+  $('#'+classAdd).addClass("btn-success");  
+}
+
+function goNext_plans(selectedValue,classRemove,classAdd){
+ // Show Hide
+ $("body").css("height", "100%");  
+ $("#personalDetails").hide();
+ $("#healthCareNeeds").hide();
+ $("#viewEstimate").hide(); 
+ $("#viewEstimatedup1").hide(); 
+ $("#viewEstimatedup2").hide(); 
+ $("#viewPlans").hide(); 
+ $('#'+selectedValue).show();
+ // Show Hide end
+ // Color Class Removing
+ $('#'+classRemove).removeClass("btn-success");
+ // Color Class Adding
+ $('#'+classAdd).addClass("btn-success");
+}
